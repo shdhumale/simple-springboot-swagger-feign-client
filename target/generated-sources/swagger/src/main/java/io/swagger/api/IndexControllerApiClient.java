50eacbf8f1cd108e0b146a6d7afe944024bfc675
shdhumale <shdhumale@gmail.com>
2021-07-16 15:11:07 +0530

    First commit

---
+++ target/generated-sources/swagger/src/main/java/io/swagger/api/IndexControllerApiClient.java
@@ -0,0 +1,8 @@
+package io.swagger.api;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import io.swagger.configuration.ClientConfiguration;
+
+@FeignClient(name="${thisIsTitleForSiddhuSwaggerExample.name:thisIsTitleForSiddhuSwaggerExample}", url="${thisIsTitleForSiddhuSwaggerExample.url:https://localhost:8080}", configuration = ClientConfiguration.class)
+public interface IndexControllerApiClient extends IndexControllerApi {
+}
